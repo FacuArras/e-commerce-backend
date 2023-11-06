ad61a7579446648304ab30578da950ff421b314e
facundo <facundoarrascaetacba@gmail.com>
2023-11-06 20:43:45 -0300

    getMerchantOrder function fixed

--- lib/mercadopago.ts
+++ lib/mercadopago.ts
@@ -3,9 +3,23 @@ import { MercadoPagoConfig, MerchantOrder } from "mercadopago";
 const client = new MercadoPagoConfig({ accessToken: process.env.MP_TOKEN, options: { timeout: 5000 } });
 
 export async function getMerchantOrder(id) {
-    const merchantOrder = new MerchantOrder(client);
-    const response = await merchantOrder.get(id);
-    return response;
+    /*  const merchantOrder = new MerchantOrder(client);
+     const response = await merchantOrder.get(id);
+     return response; */
+    try {
+        const preference = await fetch('https://api.mercadopago.com/checkout/preferences/' + id, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + process.env.MP_TOKEN
+            },
+        });
+
+        const dataRes = await preference.json();
+
+        return dataRes;
+    } catch (error) {
+        throw ('Error al obtener la preferencia en MercadoPago: ' + error.message);
+    }
 };
 
 export async function createPreference(data) {
